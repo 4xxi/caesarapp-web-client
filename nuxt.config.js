95ce1cd03f0635154a8406af410b09f8b7af4416
Alexander Kamensky <kamenskogo@gmail.com>
2018-01-30 19:49:05 +0300

    [edit]CAES-1: disabled custom.css [edit]CAES-1: added github link with styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ module.exports = {
       },
     ],
     link: [
-      {rel: 'stylesheet', href: '/assets/custom.css'},
+      /* {rel: 'stylesheet', href: '/assets/custom.css'}, */
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
       {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Rubik'},
       {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Pangolin'}
