7b0b3e014fda1bdc8c05579995a5cb8cbde9cdd0
Alexander Kamensky <kamenskogo@gmail.com>
2018-02-19 14:24:41 +0300

    [edit]CAES-1: made paranoia great again

--- pages/newdesign/throw-pilums.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <main class="page page_modal">
-    <div class="page__wrapper page__wrapper_modal aux_throw-pilums">
-      <img src="~/assets/img/hand.svg" alt="hand with pilum" class="aux__icon">
-      <div class="aux__desc aux__desc_blue">Throw pilums!</div>
-    </div>
-  </main>
-</template>
